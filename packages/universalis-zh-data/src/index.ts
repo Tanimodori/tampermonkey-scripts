edf7604c60892eb80d9f639265d7e8f96bd9760a
Tanimodori <unknowner2014@gmail.com>
2026-01-03 10:35:44 +0800

    feat: correct category

--- packages/universalis-zh-data/src/index.ts
+++ packages/universalis-zh-data/src/index.ts
@@ -1,5 +1,15 @@
 import { injectFetch } from "./hooks";
-import { Package, PackageInjector } from "./types";
+import { SearchCategory, UICategory } from "./ItemCategory";
+import type {
+  GarlandItem,
+  GarlandItemResponse,
+  GarlandSearchItem,
+  ItemCategory,
+  Package,
+  PackageInjector,
+  XIVAPIItemResponse,
+  XIVAPIItemResult,
+} from "./types";
 
 const isCafeMakerPackage = (pkg: Package) => {
   const url = new URL(pkg.url);
@@ -14,17 +24,137 @@ const isCafeMakerPackage = (pkg: Package) => {
   return false;
 };
 
+const getIconUrl = (iconId: number): string => {
+  const padded = iconId.toString().padStart(6, "0");
+  return `/i/${padded.substring(0, 3)}000/${padded}.png`;
+};
+
+const getItemCategory = (UICategoryId: number): ItemCategory => {
+  const category: ItemCategory = {
+    Icon: -1,
+    UICategory: UICategoryId,
+    UICategoryName: "",
+    SearchCategory: -1,
+    SearchCategoryName: "",
+    ParentCategory: -1,
+    ParentCategoryName: "",
+  };
+
+  // Search in UICategory
+  const target = UICategory[UICategoryId];
+  if (target) {
+    category.Icon = target.Icon;
+    category.UICategoryName = target.UICategoryName;
+  }
+
+  // Search in SearchCategory
+  if (category.Icon !== 0) {
+    for (const target of Object.values(SearchCategory)) {
+      // match by Icon
+      if (target.Icon === category.Icon) {
+        category.SearchCategory = target.SearchCategory;
+        category.SearchCategoryName = target.SearchCategoryName;
+        category.ParentCategory = target.ParentCategory;
+        const parent = SearchCategory[category.ParentCategory];
+        if (parent) {
+          category.ParentCategoryName = parent.SearchCategoryName;
+        }
+      }
+    }
+  }
+
+  return category;
+};
+
+const searchGarlandItem = async (
+  item: GarlandSearchItem,
+): Promise<XIVAPIItemResult | null> => {
+  const result: XIVAPIItemResult = {
+    ID: item.id,
+    Icon: "",
+    ItemKind: {
+      Name: "",
+    },
+    ItemSearchCategory: {
+      ID: -1,
+      Name: "",
+    },
+    LevelItem: item.obj.l,
+    Name: item.obj.n,
+    Rarity: item.obj.r ?? 0,
+  };
+
+  try {
+    const GARLAND_API_ITEM_ENDPOINT = `https://www.garlandtools.cn/db/doc/item/chs/3/${item.id}.json`;
+    const response = await fetch(GARLAND_API_ITEM_ENDPOINT);
+    const json: GarlandItemResponse = await response.json();
+    const itemDetail: GarlandItem = json.item;
+
+    if (itemDetail.tradeable !== 1) {
+      return null;
+    }
+
+    // set fields
+    result.Icon = getIconUrl(itemDetail.icon);
+    result.Rarity = itemDetail.rarity;
+
+    // category mapping
+    const category = getItemCategory(itemDetail.category);
+    result.ItemKind.Name = category.UICategoryName;
+    result.ItemSearchCategory.ID = category.SearchCategory;
+    result.ItemSearchCategory.Name = category.SearchCategoryName;
+  } catch (e) {
+    console.error("Failed to parse Garland API response:", e);
+  }
+
+  return result;
+};
+
+const searchGarland = async (searchString: string) => {
+  const newParams = new URLSearchParams({
+    text: searchString,
+    lang: "chs",
+    type: "item",
+  });
+
+  const GARLAND_API_SEARCH_ENDPOINT =
+    "https://www.garlandtools.cn/api/search.php";
+
+  const response = await fetch(
+    `${GARLAND_API_SEARCH_ENDPOINT}?${newParams.toString()}`,
+  );
+  const data: GarlandSearchItem[] = await response.json();
+
+  const result = await Promise.all(
+    data.map(async (item) => await searchGarlandItem(item)),
+  );
+
+  return result.filter((item) => item) as XIVAPIItemResult[];
+};
+
 const processPackage: PackageInjector = async (pkg) => {
   if (!isCafeMakerPackage(pkg)) {
     return pkg.response;
   }
 
+  const json = pkg.json as XIVAPIItemResponse;
   const searchParams = new URL(pkg.url).searchParams;
   const searchString = searchParams.get("string") || "";
 
-  console.log(
-    `[universalis-zh-data] processing CafeMaker package: ${searchString}`,
-  );
+  // fetch item IDs from Garland API
+  const result = await searchGarland(searchString);
+  if (result.length > 0) {
+    const resultJson: XIVAPIItemResponse = {
+      ...json,
+      Pagination: {
+        ...json.Pagination,
+        Results: result.length,
+        ResultsTotal: result.length,
+      },
+      Results: result,
+    };
+    return new Response(JSON.stringify(resultJson));
+  }
 
   // fallback
   return pkg.response;
--- packages/universalis-zh-data/src/types.ts
+++ packages/universalis-zh-data/src/types.ts
@@ -8,6 +8,16 @@ export interface Package<T = object> {
 
 export type PackageInjector = (pkg: Package) => Promise<Response>;
 
+export interface ItemCategory {
+  Icon: number;
+  UICategory: number;
+  UICategoryName: string;
+  SearchCategory: number;
+  SearchCategoryName: string;
+  ParentCategory: number;
+  ParentCategoryName: string;
+}
+
 // Garland API types
 export interface GarlandSearchItem {
   id: number;
@@ -19,7 +29,81 @@ export interface GarlandSearchItem {
     j?: number | null;
     t: number;
     l: number;
+    r?: number;
+    g?: number;
   };
 }
 
+export interface NameDescObject {
+  name: string;
+  description: string;
+}
+
+export interface GarlandItem {
+  name: string;
+  description: string;
+  jobCategories: string;
+  id: number;
+  en: NameDescObject;
+  ja: NameDescObject;
+  fr: NameDescObject;
+  de: NameDescObject;
+  patch: number;
+  category: number;
+  price: number;
+  ilvl: number;
+  dyecount: number;
+  tradeable?: number;
+  sell_price: number;
+  rarity: number;
+  stackSize: number;
+  repair: number;
+  equip: number;
+  repair_item: number;
+  glamourous: number;
+  slot: number;
+  elvl: number;
+  jobs: number;
+  models: string[];
+  attr: Record<string, number>;
+  icon: number;
+  vendors: number[];
+  upgrades: number[];
+}
+
+export interface GarlandItemResponse {
+  item: GarlandItem;
+  partials: GarlandSearchItem[];
+}
+
 // XIVAPI types
+export interface XIVAPIPagination {
+  Page: number;
+  PageNext: number | null;
+  PagePrev: number | null;
+  PageTotal: number;
+  Results: number;
+  ResultsPerPage: number;
+  ResultsTotal: number;
+}
+
+export interface XIVAPIItemResult {
+  ID: number;
+  Icon: string;
+  ItemKind: {
+    Name: string;
+  };
+  ItemSearchCategory: {
+    ID: number;
+    Name: string;
+  };
+  LevelItem: number;
+  Name: string;
+  Rarity: number;
+}
+
+export interface XIVAPIItemResponse {
+  Pagination: XIVAPIPagination;
+  Results: XIVAPIItemResult[];
+  SpeedMs: number;
+}
